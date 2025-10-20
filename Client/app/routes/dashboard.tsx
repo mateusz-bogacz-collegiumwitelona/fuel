716e20a928676ca4af375d02b05ccd3069a67dfe
Chimek006 <mateusz.chimkowski@onet.pl>
2025-10-20 09:11:23 +0200

    fast import fix in dashboard and login

--- Client/app/routes/dashboard.tsx
+++ Client/app/routes/dashboard.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../Components/header";
+import Footer from "../Components/footer";
 
 // ====================
 // Dashboard z zapytaniami do backendu:
--- Client/app/routes/login.tsx
+++ Client/app/routes/login.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Header from "../Components/header";
+import Footer from "../Components/footer";
 
 function parseJwt(token: string) {
   try {
