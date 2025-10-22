172c14be3cb2ed472fb4c3c4919b22a10554bab2
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2025-10-23 00:33:00 +0200

    validation with dataAnnotations

--- DTO/Requests/GetPaggedRequest.cs
+++ DTO/Requests/GetPaggedRequest.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DTO.Requests
 {
     public class GetPaggedRequest
     {
+        [Range(1, int.MaxValue, ErrorMessage = "Page number must be greater than 0")]
         public int? PageNumber { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Page size must be greater than 0")]
         public int? PageSize { get; set; }
     }
 }
--- DTO/Requests/GetStationListRequest.cs
+++ DTO/Requests/GetStationListRequest.cs
@@ -1,18 +1,27 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace DTO.Requests
 {
     public class GetStationListRequest
     {
         //location
+        [Range(-90, 90, ErrorMessage = "Latitude must be between -90 and 90.")]
         public double? LocationLatitude { get; set; }
+
+        [Range(-180, 180, ErrorMessage = "Longitude must be between -180 and 180.")]
         public double? LocationLongitude { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Distance must be a positive value")]
         public double? Distance { get; set; }
 
         //fuelType
         public List<string>? FuelType { get; set; }
 
         //price 
+        [Range(0, double.MaxValue, ErrorMessage = "Minimum price cannot be negative")]
         public decimal? MinPrice { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Maximum price cannot be negative")]
         public decimal? MaxPrice { get; set; }
 
         //brand
@@ -20,7 +29,10 @@ namespace DTO.Requests
 
         //sorting by
         public bool? SortingByDisance { get; set; } = false;
+        
         public bool? SortingByPrice { get; set; } = false;
+
+        [RegularExpression("^(asc|desc)$", ErrorMessage = "Sorting direction must be 'asc' or 'desc'")]
         public string? SortingDirection { get; set; } = "asc";
 
         public GetPaggedRequest? Pagging { get; set; }
