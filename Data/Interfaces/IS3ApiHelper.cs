d0939bce3c02fe0dd2833ec55c766bca8551bd68
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2025-11-30 17:01:03 +0100

    delete unused function

--- Data/Interfaces/IS3ApiHelper.cs
+++ Data/Interfaces/IS3ApiHelper.cs
@@ -5,6 +5,5 @@
         string GetPublicUrl(string objectPath, string? bucketName = null);
         Task<string> UploadFileAsync(Stream photo, string fileName, string contentType, string? bucketName = null, string? subPath = null);
         Task<bool> DeleteFileAsync(string objectPath, string? bucketName = null);
-        Task<string> GetPresignedUrlAsync(string objectPath, string bucketName);
     }
 }
