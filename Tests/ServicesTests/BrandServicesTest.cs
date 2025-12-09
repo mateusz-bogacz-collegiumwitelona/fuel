5d8829a42d6472aa83e167b39f63ac86e9a2e464
Szymon Kazimierz Mikołajek <szymon.mikolajek@studenci.collegiumwitelona.pl>
2025-12-09 21:36:46 +0100

    OK

---
+++ Tests/ServicesTests/BrandServicesTest.cs
@@ -0,0 +1,43 @@
+﻿using Data.Context;
+using Data.Interfaces;
+using Data.Models;
+using Data.Reopsitories;
+using DTO.Requests;
+using DTO.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using Services.Helpers;
+using Services.Services;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Tests.ServicesTests
+{
+    public class BrandServicesTest
+    {
+        private readonly IBrandRepository _brandRepository;
+        private readonly ILogger<BrandServices> _logger;
+        private readonly CacheService _cache;
+
+        public BrandServicesTest(
+            IBrandRepository brandRepository,
+            ILogger<BrandServices> logger,
+            CacheService cache
+            )
+        {
+            _brandRepository = brandRepository;
+            _logger = logger;
+            _cache = cache;
+        }
+    }
+}
+
---
+++ Tests/ServicesTests/ProposalStatisticServicesTest.cs
@@ -0,0 +1,67 @@
+﻿using Data.Interfaces;
+using Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Services.Helpers;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;   
+using Xunit;
+
+namespace Tests.ServicesTests
+{
+    public class ProposalStatisticServicesTest
+    {
+        private readonly IProposalStatisticRepository _proposalStatisticRepository;
+        private readonly ILogger<ProposalStatisticServices> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly CacheService _cache;
+        public ProposalStatisticServicesTest(
+            IProposalStatisticRepository proposalStatisticRepository,
+            ILogger<ProposalStatisticServices> logger,
+            UserManager<ApplicationUser> userManager,
+            CacheService cache
+            )
+        {
+            _proposalStatisticRepository = proposalStatisticRepository;
+            _logger = logger;
+            _userManager = userManager;
+            _cache = cache;
+        }
+        [Fact]
+        public async Task GetUserProposalStatisticResponse_SuccessIfResponseIsValid()
+        {
+            //Arrange
+            var service = new ProposalStatisticServices(
+                _proposalStatisticRepository,
+                _logger,
+                _userManager,
+                _cache
+                );
+            var email = "";
+            //Act
+            var result = await service.GetUserProposalStatisticResponse(email);
+            //Assert
+            Assert.True(result.IsSuccess);
+        }
+        [Fact] 
+        public async Task GetUserProposalStatisticResponse_FailIfEmailIsNullOrEmpty()
+        {
+            //Arrange
+            var service = new ProposalStatisticServices(
+                _proposalStatisticRepository,
+                _logger,
+                _userManager,
+                _cache
+                );
+            string email = null;
+            //Act
+            var result = await service.GetUserProposalStatisticResponse(email);
+            //Assert
+            Assert.False(result.IsSuccess);
+        }
+    } 
+}
