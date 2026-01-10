35dddd97b6429085872da01ef4e034e18f1627e8
mateusz-bogacz-collegiumwitelona <mateusz.bogacz-drewniak@studenci.collegiumwitelona.pl>
2026-01-10 22:58:07 +0100

    repair program.cs

--- Controllers/Program.cs
+++ Controllers/Program.cs
@@ -255,7 +255,6 @@ builder.Services.AddSingleton(sp =>
 //configure facebook auth settings
 builder.Services.Configure<Microsoft.AspNetCore.Authentication.Google.GoogleOptions>(
     builder.Configuration.GetSection("Google"));
-
 //register repo 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IStationRepository, StationRepository>();
@@ -278,18 +277,37 @@ builder.Services.AddScoped<IBrandServices, BrandServices>();
 builder.Services.AddScoped<IBanService, BanService>();
 builder.Services.AddScoped<IReportService, ReportService>();
 
+//singleton email queue
+builder.Services.AddSingleton<IEmailQueue, InMemoryEmailQueue>();
+
 //register helpers
 builder.Services.AddScoped<IEmailSender, EmailSender>();
 builder.Services.AddScoped<EmailBodys>();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<ITokenFactory, TokenFactory>();
 builder.Services.AddScoped<CacheService>();
-builder.Services.AddScoped<IStorage, BlobApiHelper>(); 
+builder.Services.AddScoped<IStorage, BlobApiHelper>();
 builder.Services.AddScoped<GoogleAuthClient>();
 
 //register background services
 builder.Services.AddHostedService<BanExpirationService>();
 builder.Services.AddHostedService<ProposalExpirationService>();
+builder.Services.AddHostedService<EmailBackgroundWorker>();
+
+//register dispacher
+builder.Services.AddTransient<IEventDispatcher, EventDispatcher>();
+
+//register observers
+builder.Services.AddTransient<IEventHandler<PriceProposalEvaluatedEvent>, UpdateUserStatisticsHandler>();
+builder.Services.AddTransient<IEventHandler<PriceProposalEvaluatedEvent>, ProposalEmailNotificationHandler>();
+builder.Services.AddTransient<IEventHandler<PriceProposalEvaluatedEvent>, ProposalCacheInvalidationHandler>();
+builder.Services.AddTransient<IEventHandler<UserRegisteredEvent>, InitializeUserStatsHandler>();
+builder.Services.AddTransient<IEventHandler<UserRegisteredEvent>, SendRegistrationEmailHandler>();
+builder.Services.AddTransient<IEventHandler<UserBannedEvent>, ClearUserReportsHandler>();
+builder.Services.AddTransient<IEventHandler<UserBannedEvent>, NotifyUserBanHandler>();
+builder.Services.AddTransient<IEventHandler<UserBannedEvent>, InvalidateBannedUserCacheHandler>();
+builder.Services.AddTransient<IEventHandler<UserUnlockedEvent>, NotifyUserUnlockHandler>();
+builder.Services.AddTransient<IEventHandler<UserUnlockedEvent>, InvalidateUnlockedUserCacheHandler>();
 
 builder.Services.AddControllers(op =>
 {
